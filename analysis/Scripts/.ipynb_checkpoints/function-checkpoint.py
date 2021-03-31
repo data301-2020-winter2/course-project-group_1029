a80b5fc8c38e290f96a34175ac0c03835c9d6d2e
amritasidhu <amritasidhu@live.com>
2021-03-31 01:43:07 -0700

    in progress

--- analysis/Scripts/.ipynb_checkpoints/function-checkpoint.py
+++ analysis/Scripts/.ipynb_checkpoints/function-checkpoint.py
@@ -3,35 +3,30 @@ import numpy as np
 import seaborn as sns
 
 #Wrapping method chains in a function:
-def load_and_process(url_or_path_to_csv_file):
-
+def load_process(url_or_path_to_csv_file):
+    
+    
     # Method Chain 1 (Load data and deal with missing data)
-
-    df1 = (
-            pd.read_csv(url_or_path_to_csv_file)
+    data = (pd.read_csv(url_or_path_to_csv_file) 
             .dropna()
             .drop(columns =['Unnamed: 0',' 0'])
-      )
-
+            .drop([' 77516',' Never-married',' 2174',' 13'], axis =1 )
+        
+        )
     # Method Chain 2 (Create new columns, drop others, and do processing)
-
-    df2 = (
-        df1
+    data2 = (
+        data
         .rename(columns= {'39': 'Age'})
-        .rename(columns= {' 77516': 'fnlwgt'})
-        .rename(columns= {'education': 'Education'})
-        .rename(columns= {' 13': 'Education-Num'})
-        .rename(columns= {' Never-married': 'Marital-Status'})
+        .rename(columns= {' Bachelors': 'Education'})
+        .rename(columns={' State-gov': 'Work Class'})
         .rename(columns= {' Adm-clerical': 'Occupation'})
         .rename(columns= {' Not-in-family': 'Relationship'})
         .rename(columns= {' White': 'Race'})
         .rename(columns= {' Male': 'Sex'})
-        .rename(columns= {' 2174': 'Capital-Gain'})
         .rename(columns= {' 40': 'Hours/Week'})
         .rename(columns= {' United-States': 'Native Country'})
-      )
-
-    # Make sure to return the latest dataframe
-
-    return df2 
-    load_and_process(r"C:\Users\Amrita\Desktop\Data301\course-project-group_1029\data\raw\adult.data")
+    )
+    
+  
+    return data
+  
